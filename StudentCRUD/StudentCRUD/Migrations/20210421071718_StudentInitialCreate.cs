21e88e2e67ca714d837d2f49d4e87f56e78a3034
Hridoy <hridoyswe205@gmail.com>
2021-04-21 13:51:06 +0600

    Student CRUD Operation

---
+++ StudentCRUD/StudentCRUD/Migrations/20210421071718_StudentInitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentCRUD.Migrations
+{
+    public partial class StudentInitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "studentModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(nullable: false),
+                    EmailAddress = table.Column<string>(nullable: false),
+                    Mobile = table.Column<string>(nullable: false),
+                    Address = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_studentModels", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "studentModels");
+        }
+    }
+}
