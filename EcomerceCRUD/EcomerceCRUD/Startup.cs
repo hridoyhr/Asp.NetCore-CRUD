8444fff93fe13ee56f3d7233d46ac564b269e8c4
Hridoy <hridoyswe205@gmail.com>
2021-04-21 22:47:28 +0600

    All Process Done

--- EcomerceCRUD/EcomerceCRUD/Startup.cs
+++ EcomerceCRUD/EcomerceCRUD/Startup.cs
@@ -55,7 +55,7 @@ namespace EcomerceCRUD
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=EComerce}/{action=Index}/{id?}");
             });
         }
     }
